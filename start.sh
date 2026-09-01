302d644c0ea8bd50160c2465aed78c2ecb2d3ff5
ASHALIVE <bdiptvworld@gmail.com>
2026-09-01 23:35:00 +0600

    Enhance FFmpeg stream pipeline with new filters
    Refactor FFmpeg command to include protocol whitelist and add auto-scaling filter complex.

--- start.sh
+++ start.sh
@@ -3,15 +3,16 @@
 # Start Nginx
 nginx
 
-# Set configuration variables
+# Configuration
 LOGO_URL="https://raw.githubusercontent.com/0ashalive/btc/main/logo.png"
 PLAYLIST_FILE="/start/playlist.txt"
 TEXT_FILE="/start/text.txt"
 
-# Run FFmpeg stream pipeline
-ffmpeg -re -f concat -safe 0 -stream_loop -1 -i "$PLAYLIST_FILE" -i "$LOGO_URL" \
+# Protocol Whitelist & Auto-Scaling Filter Complex
+ffmpeg -re -protocol_whitelist file,http,https,tcp,tls,crypto \
+  -f concat -safe 0 -stream_loop -1 -i "$PLAYLIST_FILE" -i "$LOGO_URL" \
   -filter_complex \
-  "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2[scaled_v]; \
+  "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,fps=30[scaled_v]; \
    [scaled_v][1:v]overlay=main_w-overlay_w-20:20[v1]; \
    [v1]drawbox=y=ih-80:color=black@0.7:width=iw:height=80:t=fill[v2]; \
    [v2]drawtext=textfile='$TEXT_FILE':reload=1:fontcolor=white:fontsize=30:y=h-55:x='w-mod(t*100\,w+tw)':fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf[outv]" \
@@ -19,3 +20,4 @@ ffmpeg -re -f concat -safe 0 -stream_loop -1 -i "$PLAYLIST_FILE" -i "$LOGO_URL" 
   -c:v libx264 -preset ultrafast -b:v 2000k -c:a aac -b:a 128k \
   -f hls -hls_time 6 -hls_list_size 5 -hls_flags delete_segments \
   /var/www/hls/live.m3u8
+  
