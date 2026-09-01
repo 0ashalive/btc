dac03c31f35de4d9c660752ff1411c4bb42e1d13
ASHALIVE <bdiptvworld@gmail.com>
2026-09-02 00:17:16 +0600

    Increase logo scale from 150 to 300

--- start.sh
+++ start.sh
@@ -22,7 +22,7 @@ ffmpeg -re -protocol_whitelist file,http,https,tcp,tls,crypto \
   -f concat -safe 0 -stream_loop -1 -i "$TEMP_PLAYLIST" -i "$LOGO_URL" \
   -filter_complex \
   "[0:v]scale=1280:720:force_original_aspect_ratio=decrease,pad=1280:720:(ow-iw)/2:(oh-ih)/2,fps=30[scaled_v]; \
-   [1:v]scale=150:-1[scaled_logo]; \
+   [1:v]scale=300:-1[scaled_logo]; \
    [scaled_v][scaled_logo]overlay=main_w-overlay_w-20:20[v1]; \
    [v1]drawbox=y=ih-80:color=black@0.7:width=iw:height=80:t=fill[v2]; \
    [v2]drawtext=textfile='$TEXT_FILE':reload=1:fontcolor=white:fontsize=30:y=h-55:x='w-mod(t*100\,w+tw)':fontfile=/usr/share/fonts/dejavu/DejaVuSans-Bold.ttf[outv]" \
